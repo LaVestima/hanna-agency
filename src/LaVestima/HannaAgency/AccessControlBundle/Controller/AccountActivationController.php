062a5ce3990b3b075d9ea4095df9f72af87d333e
Szymon Grabia <szymon.grabia@gmail.com>
2017-03-25 15:15:59 +0100

    Check token expiration time when activating account

--- src/LaVestima/HannaAgency/AccessControlBundle/Controller/AccountActivationController.php
+++ src/LaVestima/HannaAgency/AccessControlBundle/Controller/AccountActivationController.php
@@ -8,26 +8,31 @@
 
 namespace LaVestima\HannaAgency\AccessControlBundle\Controller;
 
-
+use LaVestima\HannaAgency\AccessControlBundle\Entity\Tokens;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class AccountActivationController extends Controller {
+	// TODO: add flashes
 	public function indexAction(string $activationToken) {
 		$token = $this->getDoctrine()->getRepository('AccessControlBundle:Tokens')
 			->findOneBy(['token' => $activationToken]);
 
 		if ($token) {
-			$user = $token->getIdUsers();
-			$user->setIdRoles($this->getDoctrine()
-				->getRepository('UserManagementBundle:Roles')
-				->findOneBy(['code' => 'ROLE_USER'])
-			);
-			
-			$em = $this->getDoctrine()->getManager();
-			$token->setDateExpired(new \DateTime('now'));
-			$em->flush();
+			if ($token->getDateExpired() < (new \DateTime('now'))) {
+				$message = 'Token expired';
+			}
+			else {
+				$user = $token->getIdUsers();
+				$userRole = $this->getDoctrine()->getRepository('UserManagementBundle:Roles')
+					->findOneBy(['code' => 'ROLE_USER']);
+				$user->setIdRoles($userRole);
 
-			$message = 'User ok!';
+				$this->disableToken($token);
+				$em = $this->getDoctrine()->getManager();
+				$em->flush();
+
+				$message = 'User ok!';
+			}
 		}
 		else {
 			$message = 'Wrong token!';
@@ -37,4 +42,11 @@ class AccountActivationController extends Controller {
 			'message' => $message
 		]);
 	}
+
+	private function disableToken(Tokens $token) {
+		$token->setDateExpired(new \DateTime('now'));
+		$this->getDoctrine()->getManager()->flush();
+	}
+
+
 }
