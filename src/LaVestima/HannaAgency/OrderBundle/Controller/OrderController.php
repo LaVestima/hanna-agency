04ed8ea84f9240313bb7024c33ba2c26a709f058
Szymon Grabia <szymon.grabia@gmail.com>
2017-05-15 18:55:12 +0200

    Add ability to delete orders

--- src/LaVestima/HannaAgency/OrderBundle/Controller/OrderController.php
+++ src/LaVestima/HannaAgency/OrderBundle/Controller/OrderController.php
@@ -80,4 +80,34 @@ class OrderController extends BaseController {
 
 	    return $this->render('@Order/Order/new.html.twig');
     }
+
+    public function deleteAction(string $pathSlug) {
+	    $order = $this->get('order_crud_controller')
+            ->readOneEntityBy(['pathSlug' => $pathSlug]);
+
+	    if (!$order) {
+	        // TODO: change
+	        var_dump('errorrrrr');
+	        die();
+        }
+
+        if ($this->get('security.authorization_checker')->isGranted('ROLE_ADMIN')
+            || $order->getIdCustomers() === $this->getCustomer()) {
+            $this->get('order_crud_controller')
+                ->deleteEntity($order);
+
+            $this->addFlash(
+                'notice',
+                'Order deleted!'
+            );
+        }
+        else {
+            $this->addFlash(
+                'warning',
+                'No order found!'
+            );
+        }
+
+        return $this->redirectToRoute('order_list');
+    }
 }
