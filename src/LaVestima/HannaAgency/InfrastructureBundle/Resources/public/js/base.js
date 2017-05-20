bb54b841c6bb5c5c4c1a5c5eae0a5986bcc68e6c
Szymon Grabia <szymon.grabia@gmail.com>
2017-05-21 00:19:14 +0200

    Add possibility to collapse module boxes

--- src/LaVestima/HannaAgency/InfrastructureBundle/Resources/public/js/base.js
+++ src/LaVestima/HannaAgency/InfrastructureBundle/Resources/public/js/base.js
@@ -2,4 +2,8 @@ $(function() {
     $('.flash-message').click(function() {
         $(this).fadeOut('slow');
     });
+
+    $('.module-box-title').click(function () {
+        $(this).parent().find('.module-box-content').toggleClass('hidden');
+    });
 });
