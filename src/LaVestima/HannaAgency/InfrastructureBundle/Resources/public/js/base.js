7b31e65d8077818fa1553ee669542570af1eb48f
Szymon Grabia <szymon.grabia@gmail.com>
2017-05-26 17:33:11 +0200

    Add animation for flash messages

--- src/LaVestima/HannaAgency/InfrastructureBundle/Resources/public/js/base.js
+++ src/LaVestima/HannaAgency/InfrastructureBundle/Resources/public/js/base.js
@@ -6,4 +6,12 @@ $(function() {
     $('.module-box-title').click(function () {
         $(this).parent().find('.module-box-content').toggleClass('hidden');
     });
+
+
+    $('.flash-message-list').delay(500)
+        .animate({
+            top: "0px"
+        }, 500)
+        .delay(3000)
+        .fadeOut('slow');
 });
