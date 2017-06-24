0d779dbd58945e5b703a4b30d475e78357a311e6
Szymon Grabia <szymon.grabia@gmail.com>
2017-06-24 20:25:19 +0200

    Add User to Login_Attempts if the attempt is failed

--- src/LaVestima/HannaAgency/AccessControlBundle/Handler/AuthenticationHandler.php
+++ src/LaVestima/HannaAgency/AccessControlBundle/Handler/AuthenticationHandler.php
@@ -4,6 +4,7 @@ namespace LaVestima\HannaAgency\AccessControlBundle\Handler;
 
 use LaVestima\HannaAgency\AccessControlBundle\Controller\Crud\LoginAttemptCrudController;
 use LaVestima\HannaAgency\AccessControlBundle\Entity\LoginAttempts;
+use LaVestima\HannaAgency\UserManagementBundle\Controller\Crud\UserCrudController;
 use Symfony\Bundle\FrameworkBundle\Routing\Router;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,21 +13,27 @@ use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\Authentication\AuthenticationFailureHandlerInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
 
-class AuthenticationHandler
-implements AuthenticationSuccessHandlerInterface,
-AuthenticationFailureHandlerInterface {
+class AuthenticationHandler implements
+    AuthenticationSuccessHandlerInterface,
+    AuthenticationFailureHandlerInterface
+{
     private $loginAttemptCrudController;
+    private $userCrudController;
     private $router;
 
     public function __construct(
             LoginAttemptCrudController $loginAttemptCrudController,
-            Router $router) {
+            UserCrudController $userCrudController,
+            Router $router
+    ) {
         $this->loginAttemptCrudController = $loginAttemptCrudController;
+        $this->userCrudController = $userCrudController;
         $this->router = $router;
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token) {
         $loginAttempt = new LoginAttempts();
+
         $loginAttempt->setIpAdddress($request->getClientIp());
         $loginAttempt->setIsFailed(0);
         $loginAttempt->setIdUsers($token->getUser());
@@ -41,8 +48,15 @@ AuthenticationFailureHandlerInterface {
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception) {
         $loginAttempt = new LoginAttempts();
+
+        $user = $this->userCrudController
+            ->readOneEntityBy([
+                'login' => $exception->getToken()->getUser()
+            ]);
+        
         $loginAttempt->setIpAdddress($request->getClientIp());
         $loginAttempt->setIsFailed(1);
+        $loginAttempt->setIdUsers($user);
 
         $this->loginAttemptCrudController
             ->createEntity($loginAttempt);
