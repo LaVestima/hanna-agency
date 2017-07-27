11b08faf55c94841f701567fd1fbe48bc17fb677
Szymon Grabia <szymon.grabia@gmail.com>
2017-07-27 22:09:53 +0200

    Unite order_list and order_deleted_list

---
+++ src/LaVestima/HannaAgency/OrderBundle/Controller/Crud/OrderCrudControllerInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lavestima
+ * Date: 27/07/17
+ * Time: 20:26
+ */
+
+namespace LaVestima\HannaAgency\OrderBundle\Controller\Crud;
+
+
+class OrderCrudControllerInterface
+{
+
+}
--- src/LaVestima/HannaAgency/OrderBundle/Controller/OrderController.php
+++ src/LaVestima/HannaAgency/OrderBundle/Controller/OrderController.php
@@ -3,96 +3,110 @@
 namespace LaVestima\HannaAgency\OrderBundle\Controller;
 
 use LaVestima\HannaAgency\InfrastructureBundle\Controller\BaseController;
+use LaVestima\HannaAgency\OrderBundle\Controller\Crud\OrderCrudControllerInterface;
 use LaVestima\HannaAgency\OrderBundle\Entity\Orders;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 class OrderController extends BaseController
 {
-	public function listAction(Request $request)
+    private $authorizationChecker;
+    private $orderCrudController;
+
+    /**
+     * OrderController constructor.
+     *
+     * @param AuthorizationCheckerInterface $authorizationChecker
+     * @param OrderCrudControllerInterface $orderCrudController
+     */
+    public function __construct(
+        AuthorizationCheckerInterface $authorizationChecker,
+        OrderCrudControllerInterface $orderCrudController
+
+    ) {
+        $this->authorizationChecker = $authorizationChecker;
+        $this->orderCrudController = $orderCrudController;
+    }
+
+    /**
+     * Order List Action.
+     *
+     * @param Request $request
+     * @param string $type
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function listAction(Request $request, string $type)
     {
-        $authChecker = $this->get('security.authorization_checker');
-
-        $orderCrudController = $this->get('order_crud_controller');
-
-        $orderCrudController->setAlias('o');
-
-        if ($authChecker->isGranted('ROLE_ADMIN')) {
-            $orderCrudController->readAllUndeletedEntities();
+        if (!in_array($type, ['', 'deleted'])) {
+            throw new NotFoundHttpException();
         }
-        else if ($authChecker->isGranted('ROLE_CUSTOMER')) {
+
+        $this->orderCrudController->setAlias('o');
+
+        if ($this->authorizationChecker->isGranted('ROLE_ADMIN')) {
+            if ($type === '') {
+                $this->orderCrudController->readAllUndeletedEntities();
+            } elseif ($type === 'deleted') {
+                $this->orderCrudController->readAllDeletedEntities();
+            }
+        }
+        else if ($this->authorizationChecker->isGranted('ROLE_CUSTOMER')) {
             // TODO: only undeleted
-            $orderCrudController->readEntitiesBy([
+            $this->orderCrudController->readEntitiesBy([
                 'idCustomers' => $this->getCustomer()->getId()
             ]);
         }
-        else {
-//	        throw new AccessDeniedHttpException();
-            // TODO: exception ?? for ROLE_USER and lower
-        }
-        // TODO: sort orders ...
 
-        $orderCrudController
+        $this->orderCrudController
             ->join('idCustomers', 'c')
             ->orderBy('dateCreated', 'DESC');
 
         $pagination = $this->get('knp_paginator')->paginate(
-            $orderCrudController->getQuery(),
+            $this->orderCrudController->getQuery(),
             $request->query->getInt('page', 1)/*page number*/,
             10 /*limit per page*/
         );
 
         foreach ($pagination->getItems() as $order) {
-            $order->setStatus($orderCrudController->generateStatus($order));
+            $order->setStatus(
+                $this->orderCrudController->generateStatus($order)
+            );
         }
 
         return $this->render('@Order/Order/list.html.twig', [
-            'pagination' => $pagination
+            'pagination' => $pagination,
+            'listType' => $type
         ]);
-
-
-
-        // --------------------
-
-//        $orders = $orders->sortBy(['dateCreated' => 'DESC'])
-//            ->getEntities();
 	}
 
-	public function deletedListAction()
-    {
-	    $orders = $this->get('order_crud_controller')
-            ->readAllDeletedEntities()
-            ->sortBy(['datePlaced' => 'DESC'])
-            ->getEntities();
-
-	    return $this->render('@Order/Order/deletedList.html.twig', [
-	        'orders' => $orders,
-        ]);
-    }
-
+    /**
+     * Order Show Action
+     *
+     * @param string $pathSlug
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
 	public function showAction(string $pathSlug)
     {
-        $authChecker = $this->get('security.authorization_checker');
-        $orderCrudController = $this->get('order_crud_controller');
-
         $order = null;
 
-        if ($authChecker->isGranted('ROLE_ADMIN')) {
-            $order = $orderCrudController
+        if ($this->authorizationChecker->isGranted('ROLE_ADMIN')) {
+            $order = $this->orderCrudController
                 ->readOneEntityBy([
                     'pathSlug' => $pathSlug
                 ]);
         }
-        else if ($authChecker->isGranted('ROLE_CUSTOMER')) {
-            $order = $orderCrudController
+        else if ($this->authorizationChecker->isGranted('ROLE_CUSTOMER')) {
+            $order = $this->orderCrudController
                 ->readOneEntityBy([
                     'pathSlug' => $pathSlug,
-                    'idCustomers' => $this->getCustomer()
+                    'idCustomers' => $this->getCustomer()->getId()
                 ]);
         }
-        else {
-            // TODO: exception ?? for ROLE_USER and lower
-        }
+
+        $order = $this->orderCrudController->getResult();
 
 		if (!$order) {
             $this->addFlash('warning', 'No order found!');
@@ -101,8 +115,10 @@ class OrderController extends BaseController
         }
 
 		$ordersProducts = $this->get('order_product_crud_controller')
-			->readEntitiesBy(['idOrders' => $order])
-            ->getEntities();
+			->readEntitiesBy([
+			    'idOrders' => $order->getId()
+            ])
+            ->getResult();
 		
 		return $this->render('@Order/Order/show.html.twig', [
 			'order' => $order,
@@ -111,22 +127,28 @@ class OrderController extends BaseController
 	}
 
 	// TODO: delete??
-	public function newAction()
-    {
+//	public function newAction()
+//    {
+//
+//	    return $this->render('@Order/Order/new.html.twig');
+//    }
 
-	    return $this->render('@Order/Order/new.html.twig');
-    }
-
+    /**
+     * Order Delete Action
+     *
+     * @param string $pathSlug
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
     public function deleteAction(string $pathSlug)
     {
-	    $order = $this->get('order_crud_controller')
-            ->readOneEntityBy(['pathSlug' => $pathSlug]);
+	    $order = $this->orderCrudController
+            ->readOneEntityBy(['pathSlug' => $pathSlug])
+            ->getResult();
 
 	    if (!$order || !$this->isUserAllowedToViewEntity($order)) {
             $this->addFlash('warning', 'No order found!');
         } else {
-            $this->get('order_crud_controller')
-                ->deleteEntity($order);
+            $this->orderCrudController->deleteEntity($order);
 
             $this->addFlash('notice', 'Order deleted!');
         }
@@ -134,10 +156,17 @@ class OrderController extends BaseController
         return $this->redirectToRoute('order_list');
     }
 
+    /**
+     * Order Restore Action
+     *
+     * @param string $pathSlug
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
     public function restoreAction(string $pathSlug)
     {
 	    $order = $this->get('order_crud_controller')
-            ->readOneEntityBy(['pathSlug' => $pathSlug]);
+            ->readOneEntityBy(['pathSlug' => $pathSlug])
+            ->getResult();
 
 	    $this->get('order_crud_controller')
             ->restoreEntity($order);
