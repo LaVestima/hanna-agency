b613031fdde1f5ddfc0876b12ac99bf2ccd9d150
Szymon Grabia <szymon.grabia@gmail.com>
2017-10-10 12:01:03 +0200

    Add test for reading multiple Users

--- src/LaVestima/HannaAgency/UserManagementBundle/Tests/Controller/Crud/UserCrudControllerTest.php
+++ src/LaVestima/HannaAgency/UserManagementBundle/Tests/Controller/Crud/UserCrudControllerTest.php
@@ -3,17 +3,38 @@
 namespace LaVestima\HannaAgency\UserManagementBundle\Tests\Controller\Crud;
 
 use LaVestima\HannaAgency\InfrastructureBundle\Tests\BaseWebTestCase;
+use LaVestima\HannaAgency\UserManagementBundle\Entity\Users;
 
 class UserCrudControllerTest extends BaseWebTestCase
 {
     public function testReadUserFromDatabase()
     {
         $user = $this->client->getContainer()->get('user_crud_controller')
-            ->readOneEntityBy(['id' => 1]);
+            ->readOneEntityBy(['id' => 1])
+            ->getResult();
 
         $this->assertInstanceOf(
             'LaVestima\HannaAgency\UserManagementBundle\Entity\Users',
             $user
         );
+    }
+
+    public function testReadUsersFromDatabase()
+    {
+        $users = $this->client->getContainer()->get('user_crud_controller')
+            ->readEntitiesBy([
+                'id' => ['0', '>']
+            ])
+            ->getResult();
+
+        foreach ($users as $user) {
+            if (!$user instanceof Users) {
+                $this->assertTrue(false);
+
+                return;
+            }
+        }
+
+        $this->assertTrue(true);
     }
 }
