9d2bb09af8b43838e3472a0ec4eb7fa9aa3ffb56
Szymon Grabia <szymon.grabia@gmail.com>
2017-10-10 12:35:18 +0200

    Refactor limit of failed login attempts

--- src/LaVestima/HannaAgency/AccessControlBundle/Controller/LoginController.php
+++ src/LaVestima/HannaAgency/AccessControlBundle/Controller/LoginController.php
@@ -7,6 +7,18 @@ use Symfony\Component\HttpFoundation\Request;
 
 class LoginController extends Controller
 {
+    const FAILED_LOGIN_MAX_ATTEMPTS = 3;
+    const FAILED_LOGIN_RESET_TIME = 10;
+
+    /**
+     * Login Index Action.
+     *
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @throws \Exception
+     */
 	public function indexAction(Request $request)
     {
         $ipAddress = $request->getClientIp();
@@ -14,31 +26,41 @@ class LoginController extends Controller
 		$authenticationUtils = $this->get('security.authentication_utils');
 
 		$error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+        $loginDisabledMessage = null;
 
-        if (count($this->getFailedLogins($ipAddress, 6)) > 2) {
-            $error['message'] = 'Access Blocked. Please try again later!';
-            throw new \Exception('Access Blocked. Too many failed login attempts. Please try again later!');
-        } else {
-            $error['message'] = 'ha';
+        if (count($this->getFailedLogins($ipAddress, self::FAILED_LOGIN_RESET_TIME)) >= self::FAILED_LOGIN_MAX_ATTEMPTS) {
+            $loginDisabledMessage = 'Access Blocked. Too many failed login attempts. Please try again later!';
         }
 
-		$lastUsername = $authenticationUtils->getLastUsername();
-
-		return $this->render('AccessControlBundle:Login:index.html.twig', array(
+		return $this->render('AccessControlBundle:Login:index.html.twig', [
 			'last_username' => $lastUsername,
 			'error'         => $error,
-		));
+            'loginDisabled' => $loginDisabledMessage ? ['message' => $loginDisabledMessage] : null,
+		]);
 	}
 
-	private function getFailedLogins(string $ip, $hours)
+    /**
+     * Get the array of failed logins from given IP, within given minutes.
+     *
+     * @param string $ip
+     * @param $minutes
+     *
+     * @return array
+     */
+	private function getFailedLogins(string $ip, $minutes)
     {
         $failedLogins = $this->get('login_attempt_crud_controller')
             ->readEntitiesBy([
                 'ipAddress' => $ip,
-                'dateCreated' => ['now -' . $hours . ' hours', '<'],
+                'dateCreated' => [(new \DateTime('now -' . $minutes . ' minutes')), '>'],
                 'isFailed' => true,
             ])
             ->getResult();
+
+        if (!is_array($failedLogins)) {
+            $failedLogins = [$failedLogins];
+        }
 
         return $failedLogins;
     }
