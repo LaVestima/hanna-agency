f6a6c7d52024d431e4d851d69576039c3515f911
Szymon Grabia <szymon.grabia@gmail.com>
2017-10-29 23:50:33 +0100

    Create History tables

---
+++ src/LaVestima/HannaAgency/InfrastructureBundle/Controller/Action/NewActionControllerTrait.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lavestima
+ * Date: 29/10/17
+ * Time: 20:59
+ */
+
+namespace LaVestima\HannaAgency\InfrastructureBundle\Controller\Action;
+
+
+trait NewActionControllerTrait
+{
+
+}
