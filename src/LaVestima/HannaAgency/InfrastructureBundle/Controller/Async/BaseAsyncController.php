6da476ce773aed75adeaa0595b82e141db630149
Szymon Grabia <szymon.grabia@gmail.com>
2017-12-24 23:51:37 +0100

    Block direct access to asynchronous list actions

--- src/LaVestima/HannaAgency/InfrastructureBundle/Controller/Async/BaseAsyncController.php
+++ src/LaVestima/HannaAgency/InfrastructureBundle/Controller/Async/BaseAsyncController.php
@@ -4,14 +4,29 @@ namespace LaVestima\HannaAgency\InfrastructureBundle\Controller\Async;
 
 use LaVestima\HannaAgency\InfrastructureBundle\Controller\BaseController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\Intl\Exception\MethodNotImplementedException;
 
 class BaseAsyncController extends BaseController
 {
+    /**
+     * @var Request
+     */
+    protected $request;
     protected $isListDeleted = false;
 
+    /**
+     * Async List Action.
+     *
+     * @param Request $request
+     * @return mixed
+     */
     public function listAction(Request $request)
     {
+        $this->request = $request;
+
+        $this->denyNonXhrs();
+
         $this->isListDeleted = false;
 
         return $this->genericListAction($request);
@@ -25,13 +40,35 @@ class BaseAsyncController extends BaseController
      */
     public function deletedListAction(Request $request)
     {
+        $this->request = $request;
+
+        $this->denyNonXhrs();
+
         $this->isListDeleted = true;
 
         return $this->genericListAction($request);
     }
 
+    /**
+     * Async Generic List Action.
+     *
+     * @param Request $request
+     * @throws MethodNotImplementedException
+     */
     protected function genericListAction(Request $request)
     {
         throw new MethodNotImplementedException('genericListAction');
+    }
+
+    /**
+     * Deny access when request is not asynchronous.
+     *
+     * @throws AccessDeniedHttpException
+     */
+    private function denyNonXhrs()
+    {
+        if (!$this->request->isXmlHttpRequest()) {
+            throw new AccessDeniedHttpException();
+        }
     }
 }
